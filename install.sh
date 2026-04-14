14bad18917b4f21ad3bc1676225fefc122ab9984
Umair Ashraf <umrashrf@gmail.com>
2026-04-13 23:17:28 -0600

    Let's try alpine linux

--- install.sh
+++ install.sh
@@ -9,21 +9,14 @@ cd /tmp/mailexp-script
 cp -rf postfix/* /etc/postfix/
 cp -rf dovecot.conf /etc/dovecot/
 cp users /etc/dovecot/
-
 chown root:dovecot /etc/dovecot/users
 chmod 640 /etc/dovecot/users
 
-if ! getent group mail > /dev/null 2>&1; then
-    addgroup mail
-fi
-useradd -m -g mail umair
-
+adduser -D umair mail
 mkdir -p /home/umair/Maildir/cur && \
     mkdir -p /home/umair/Maildir/new && \
     mkdir -p /home/umair/Maildir/tmp
-
 chown umair:umair -R /home/umair/Maildir
-
 echo "umair:test" | chpasswd
 
 awk '{gsub(/smtp\t+25/, "smtp\t\t36245"); print}' /etc/services > /tmp/services
